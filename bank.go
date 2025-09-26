671448635b3d2de914bc0c2b40e7507cf83ad76a
meseret mekuriyaw <mekuriyawmeseret09@gmail.com>
2025-09-26 09:51:41 -0400

    then

--- bank.go
+++ bank.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import 
+(
+	"fmt"
+	"os"
+)
+func writeBalanceToFile(balance float64){
+	balanceText :=fmt.Sprint(balance)
+	os.WriteFile("balance.txt",[]byte(balanceText),0644)
+}
 
 func main() {
 	var accountBalance = 1000.0
